c02cebdf4b1fd3fbead24312cc1b2837c3a74ac6
papagotto <69018190+papagotto@users.noreply.github.com>
2023-01-20 20:21:43 -0300

    Update conexionWeebhook.js

--- Funciones/conexionWeebhook.js
+++ Funciones/conexionWeebhook.js
@@ -1,13 +1,16 @@
 function connWebhook(e, mensaje, webhook, cabeceras){
 
-var http=http();
+
+var http = http();
+logVar(http);
+exit();
 
 http.headers(cabeceras);
 var resultado = http.post(webhook,mensaje);
 
 if (resultado.code == 200){
     
-    e.set("Webhook Enviado",true)
+    e.set("Webhook Enviado",true);
     
 }  
 
