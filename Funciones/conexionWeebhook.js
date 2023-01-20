69884cbe5edd1c7163e1bb9106560d97e1bb0484
papagotto <69018190+papagotto@users.noreply.github.com>
2023-01-20 09:58:22 -0300

    Update conexionWeebhook.js

--- Funciones/conexionWeebhook.js
+++ Funciones/conexionWeebhook.js
@@ -1,34 +1,8 @@
-function connwebhook(){
-
-/*Campos
-Asunto
-Prioridad
-Vencimiento
-Estado
-Responsable
-Lugar
-*/
+function connwebhook(e,mensaje,webhook,cabeceras){
 
 var http=http();
 
-
-var len=e.length;
-var mensaje= '{ "Tareas": [ ';
-var e = selectedEntries();
-   for (i in e){
-        mensaje= mensaje + 
-	        '{"Asunto":"' + e[i].field("Asunto") +	'",' +
-	        '"Descripcion":"' + e[i].field("Descripcion") + '",' +
-	        '"Prioridad":"' + e[i].field("Prioridad") + '",' +
-	        '"Vencimiento":"' + e[i].field("Vencimiento") + '",' +
-	        '"Estado":"' + e[i].field("Estado") + '",' +
-	        '"Resposable":"' + e[i].field("Responsable") + '",' +
-	        '"Lugar":"'+ e[i].field("Lugar") +'"},'
-} 
-len=mensaje.length
-mensaje=mensaje.slice(0,len-1); //eliminamos la ultima coma
-mensaje= mensaje + ']}';
-
+  
 var cabeceras={"Content-Type":"application/json"};
 var webhook ='https://hook.us1.make.com/pmarj8ex5iiiuejtb7o56ifhom2gv89s';
 
@@ -37,14 +11,8 @@ resultado = http.post(webhook,mensaje);
 
 if (resultado.code == 200){
     
-    e.set("Weebhook Enviado?","Si")
+    e.set("Webhook Enviado","Si")
     
 }  
-//log(mensaje)
-//message(resultado.code) 
-//message(resultado.body)
-log(resultado.body)
- 
-
 
 }
